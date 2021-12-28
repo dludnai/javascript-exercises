699262232b2889a6ceb76ec912f69a16f39c9266
LDR Laptop <dludnai@yahoo.com>
2021-12-28 14:54:50 +0200

    Odin Project: Fundamentals Part 4 Exercises Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(input) {
+  let finalResult = (input-32)*(5/9);
+  if (finalResult !== 0) {
+    return (finalResult.toFixed(1));
+  }
+  else {
+    return finalResult;
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(input) {
+  let finalResult = (input*1.8)+32;
+  if (finalResult !== 0) {
+    return (finalResult.toFixed(1));
+  }
+  else {
+    return finalResult;
+  }
 };
 
 // Do not edit below this line
